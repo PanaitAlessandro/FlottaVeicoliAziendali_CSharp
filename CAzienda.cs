f75bf530d4ace107b5f19800d6160443e54874f8
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-25 18:17:50 +0200

    feat: metodi di CAzienda aggiunti

--- CAzienda.cs
+++ CAzienda.cs
@@ -5,7 +5,7 @@ namespace Flotta
     public class CAzienda
     {
         private string _ragionesociale;
-        // todo: aggiungere array flotta di CVeicolo
+        // todo: aggiungere array flotta di CVeicolo (DONE)
         private CVeicolo[] _flotta;
 
         public CVeicolo[] Flotta
@@ -40,6 +40,37 @@ namespace Flotta
         {
             ragioneSociale = ragionesociale;
             Flotta = flotta;
+        }
+
+        public void CercaPerCarburante(string tipoAlimentazione)
+        {
+            for (int i = 0; i < _flotta.Length; i++)
+            {
+                if (tipoAlimentazione == _flotta[i].tipoCarburante)
+                {
+                    System.Console.WriteLine($"Trovato: {_flotta[i]} ");
+                }
+            }
+        }
+
+        public int KilometraggioTotale()
+        {
+            int somma = 0;
+            for (int i = 0; i < _flotta.Length; i++)
+            {
+                somma += _flotta[i].Chilometraggio;
+            }
+        }
+
+        public double KilometraggioMedio()
+        {
+            double media = 0;
+            for (int i = 0; i < _flotta.Length; i++)
+            {
+                media += _flotta[i].Chilometraggio;
+            }
+            media / _flotta.Length;
+            return media;
         }
 
         public override string ToString()
